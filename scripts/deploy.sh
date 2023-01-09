1cd9c0d970c52ff8fd4fac4c05c89b09c7e761ed
Nikita Emelianov <nikita.emelian@gmail.com>
2023-01-09 20:42:00 +0300

    update deploy.sh script to fit in with bot deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # clean up
-rm -rf word-flow
+rm -rf deploy
 
 # clone repo
-git clone https://github.com/nikita-emelianov/word-flow.git
+git clone https://github.com/nikita-emelianov/word-flow.git deploy
+
+# check environment file .env-bot existence
+if ! test -f ".env-bot"; then
+  printf '\033[31m'
+  echo Error: you have to provide .env-bot file with environment variables
+  echo See deploy/bot/.env.defaults file for more info:
+  printf '\033[0m'
+  exit 1
+fi
+
+# copy environment file .env-bot to bot folder
+cp .env-bot deploy/bot/.env
 
 # run
-docker compose -f ./word-flow/docker-compose.yml up -d --build
+docker compose -f deploy/docker-compose.yml up -d --build
