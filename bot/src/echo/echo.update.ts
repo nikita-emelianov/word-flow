66462e96e46a38891b9d6601d1928ed8d1840f2f
Nikita Emelianov <nikita.emelian@gmail.com>
2023-02-02 19:08:39 +0300

    clean up on unused code

--- bot/src/echo/echo.update.ts
+++ bot/src/echo/echo.update.ts
@@ -1,5 +1,7 @@
 import { UseGuards, UseInterceptors } from '@nestjs/common';
-import { On, Message, Update, Command } from 'nestjs-telegraf';
+import { On, Message, Update, Command, Hears, Sender } from 'nestjs-telegraf';
+import { UpdateType as TelegrafUpdateType } from 'telegraf/typings/telegram-types';
+import { UpdateType } from '../common/decorators/update-type.decorator';
 import { ReverseTextPipe } from '../common/pipes/reverse-text.pipe';
 import { ResponseTimeInterceptor } from '../common/interceptors/response-time.interceptor';
 import { AdminGuard } from '../common/guards/admin.guard';
@@ -19,5 +21,13 @@ export class EchoUpdate {
     @Message('text', new ReverseTextPipe()) reversedText: string,
   ): string {
     return `Echo: ${reversedText}`;
+  }
+
+  @Hears(['hi', 'hello', 'hey', 'qq'])
+  onGreetings(
+    @UpdateType() updateType: TelegrafUpdateType,
+    @Sender('first_name') firstName: string,
+  ): string {
+    return `Hey ${firstName} ${updateType}`;
   }
 }
